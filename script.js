6a4e3cb0b7a8c2a808dd86b72a7a9f51c4f8adcc
savya sharma <savyasharma007@gmail.com>
2024-07-17 17:04:08 +0530

    Quiz program

---
+++ script.js
@@ -0,0 +1,131 @@
+// const questions = [2, 4, 6, 8, 10];
+// ARRAY OF OBJECTS
+const questions = [
+  {
+    q: "What is the capital of india?",
+    a: "New Delhi",
+    opt: ["Jaipur", "Mumbai", "New Delhi", "Kolkata"],
+  },
+  {
+    q: "Which is the national bird of India?",
+    a: "Peacock",
+    opt: ["Sparrow", "Peacock", "Parrot", "Corw"],
+  },
+  {
+    q: "Which is the 2024 Cricket T20 world cup?",
+    a: "India",
+    opt: ["India", "Australia", "South Africa", "West Indies"],
+  },
+  {
+    q: "Who is the president of India?",
+    a: "New Delhi",
+    opt: ["APJ Abdul Kalam", "Draupadi Murmu", "Rahul gandhi", "Narendra MOdi"],
+  },
+];
+
+
+const userAnswers = [];
+
+const questionDiv = document.querySelector(".question")
+const timerDiv = document.querySelector(".timer")
+const quizDiv = document.querySelector("#quiz")
+const scoreDiv = document.querySelector("#score")
+const paragraphs = document.querySelectorAll(".option")
+const score = document.querySelector("#score span");
+
+let timer = 5;
+let count = 0;
+let id1;
+let id2;
+let isQuestionAnswered = false;
+
+timerDiv.innerHTML = timer;
+// PRINT THE FIRST QUESTION INSTANTLY ON PAGE LOAD
+printQuestion();
+
+id2 = setInterval(() =>{
+    if (timer === 1) {
+        timer = 5;
+        timerDiv.innerHTML = timer;
+    } else timerDiv.innerHTML = --timer;
+}, 1000);
+
+id1 = setInterval(() =>{
+  if(count === questions.length - 1){
+    //T o check te last question
+    checkUserAnswer();
+
+    //clear the question
+    clearInterval(id1);
+    //clear  the timer
+    clearInterval(id2);
+
+    quizDiv.classList.add("hidden");
+
+    calculateScore();
+    
+    scoreDiv.classList.remove("hidden");
+  }  else{
+    // Increament the question number
+    count++;
+
+    //To check all question except the last one
+    checkUserAnswer();
+
+    //Enable all options again
+    enableAllOptions();
+
+    //Change the question
+    printQuestion();
+  }
+}, 5000);
+
+function printQuestion() {
+  questionDiv.innerHTML = questions[count].q
+  paragraphs.forEach((para, index) =>{
+    para.innerHTML = questions[count].opt[index];
+  });
+}
+
+paragraphs.forEach((para) =>{
+  para.addEventListener("click", storeUserAnswer);
+});
+
+  function storeUserAnswer(e){
+    isQuestionAnswered = true;
+    userAnswers.push(e.target.innerHTML);
+    disableAlloptions();
+    console.log(userAnswers);
+  }
+
+  function checkUserAnswer(){
+    //INSERT NULL IS USER DIDN'T ANSWER
+    if(isQuestionAnswered === flase) {
+      userAnswers.push(null);
+      console.log(userAnswers);
+    }
+    //RESET VARIABLE TO FALSE IF USER ANSWERED
+    else{
+      isQuestionAnswered = false;
+    }
+}
+
+function calculateScore() {
+  let finalScore = 0;
+  userAnswers.forEach((userAnswers, index) =>{
+    if(userAnswers === questions[index].a) finalScore++;
+  });
+  score.innerHTML = `${finalScore} / ${questions.length}`;
+}
+
+function disableAlloptions() {
+  paragraphs.forEach((para) =>{
+    para.classList.add("pointer-none")
+  })
+}
+
+function enableAllOptions() {
+  paragraphs.forEach((para) =>{
+    para.classList.remove("pointer-none");
+  });
+}
